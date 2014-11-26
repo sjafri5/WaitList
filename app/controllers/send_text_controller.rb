23d3a1c71a949b772a24e340513a8ae661ce7c3d
sjafri5 <raza.jafri.sy@gmail.com>
2014-11-26 14:43:39 -0600

    trying without secrets

--- app/controllers/send_text_controller.rb
+++ app/controllers/send_text_controller.rb
@@ -7,10 +7,10 @@ class SendTextController < ApplicationController
     @restaurant = @party.restaurant
     number_to_send_to = @party.phone
  
-    twilio_sid = Rails.application.secrets.twilio_account_sid #ENV['twilio_sid']
-    twilio_token = Rails.application.secrets.twilio_auth_token #ENV['twilio_auth_token']
+    twilio_sid = 'AC1212dc49b7f4955e9493cd368748f650' #Rails.application.secrets.twilio_account_sid #ENV['twilio_sid']
+    twilio_token = '92993d5fec98ed662deb0bcb605ebbea'#Rails.application.secrets.twilio_auth_token #ENV['twilio_auth_token']
     twilio_phone_number = "3317033062"
-    
+
     @twilio_client = Twilio::REST::Client.new twilio_sid, twilio_token
   
     @twilio_client.account.sms.messages.create(
