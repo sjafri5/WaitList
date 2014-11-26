6adfc923750702c8518cb6d588d07b3c85061233
sjafri5 <raza.jafri.sy@gmail.com>
2014-11-26 14:59:20 -0600

    fixed everything related to twilio

--- app/controllers/send_text_controller.rb
+++ app/controllers/send_text_controller.rb
@@ -4,21 +4,21 @@ class SendTextController < ApplicationController
  
   def send_text_message
     puts 'TEXT CONTROLLER ' * 100
-    # @party = Party.find(params[:id])
-    # @restaurant = @party.restaurant
-    # number_to_send_to = @party.phone
+    @party = Party.find(params[:id])
+    @restaurant = @party.restaurant
+    number_to_send_to = @party.phone
  
-    # twilio_sid = 'AC1212dc49b7f4955e9493cd368748f650' #Rails.application.secrets.twilio_account_sid #ENV['twilio_sid']
-    # twilio_token = '92993d5fec98ed662deb0bcb605ebbea'#Rails.application.secrets.twilio_auth_token #ENV['twilio_auth_token']
-    # twilio_phone_number = "3317033062"
+    twilio_sid = 'AC1212dc49b7f4955e9493cd368748f650' #Rails.application.secrets.twilio_account_sid #ENV['twilio_sid']
+    twilio_token = '92993d5fec98ed662deb0bcb605ebbea'#Rails.application.secrets.twilio_auth_token #ENV['twilio_auth_token']
+    twilio_phone_number = "3317033062"
 
-    # @twilio_client = Twilio::REST::Client.new twilio_sid, twilio_token
+    @twilio_client = Twilio::REST::Client.new twilio_sid, twilio_token
   
-    # @twilio_client.account.sms.messages.create(
-    #   :from => "+1#{twilio_phone_number}",
-    #   :to => number_to_send_to,
-    #   :body => "This is an automated message from #{@restaurant.name.capitalize}. Your table is now ready."
-    # )
+    @twilio_client.account.sms.messages.create(
+      :from => "+1#{twilio_phone_number}",
+      :to => number_to_send_to,
+      :body => "This is an automated message from #{@restaurant.name.capitalize}. Your table is now ready."
+    )
     render :json => 'Success'
   end
 end
